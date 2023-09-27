85bb1f3afd61f55fcec35d27ea434836c695da5c
prabhjot3845 <er.panerar@gmail.com>
2023-09-27 21:14:40 +0530

    branch1 question addition updated

--- backend/controllers/questionBankController.js
+++ backend/controllers/questionBankController.js
@@ -8,59 +8,13 @@ const fs = require('fs');
 
 // Create Institute -- Admin
 exports.createQuestionBank = catchAsyncErrors(async (req, res, next) => {
-//   let images = [];
 
-//   if (typeof req.body.images === "string") {
-//     images.push(req.body.images);
-//   } else {
-//     images = req.body.images;
-//   }
-
-//   const imagesLinks = [];
-
-//   for (let i = 0; i < images.length; i++) {
-//     const result = await cloudinary.v2.uploader.upload(images[i], {
-//       folder: "products",
-//     });
-
-//     imagesLinks.push({
-//       public_id: result.public_id,
-//       url: result.secure_url,
-//     });
-//   }
-
-//   req.body.images = imagesLinks;
-
-// const avatar = req.files.avatar.tempFilePath;
-
-//   const myCloud = await cloudinary.v2.uploader.upload(avatar);
-//   console.log(myCloud.url)
-  
-
-//   fs.unlink(avatar, (err) => {
-//     if (err) console.log(err);
-//   });
-  
-  // req.body.avatar = {
-  //   public_id: myCloud.public_id,
-  //   url: myCloud.secure_url,
-  // },
   req.body.candidate = req.candidate.id;
-  // req.body.email = req.user.email;
+ 
 
   const questionBank = await QuestionBank.create(req.body);
   
-  const newCandidateData = {
-    questionBankId: questionBank.id,
-  };
-
-  await Candidate.findByIdAndUpdate(req.candidate.id, newCandidateData, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
-  });
-
-  res.status(201).json({
+    res.status(201).json({
     success: true,
     questionBank,
   });
