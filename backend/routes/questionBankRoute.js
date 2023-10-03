86a0eed533d3268d113c881776ecf73d473f5073
prabhjot3845 <er.panerar@gmail.com>
2023-10-03 21:15:01 +0530

    merge 3 with branch1

--- backend/routes/questionBankRoute.js
+++ backend/routes/questionBankRoute.js
@@ -1,72 +1,40 @@
 const express = require("express");
 const {
-  getAllInstitutes,
   createQuestionBank,
-  updateInstitute,
-  deleteInstitute,
-  getInstituteDetails,
-  createInstituteReview,
-  instituteReviews,
+  getQuestion,
+  getAllQuestions,
+  updateQuestionAdmin,
+  deleteQuestion,
+  createQuestionReview,
+  questionReviews,
   deleteReview,
-  getAdminInstitutes,
-  createInstituteResult,
-  deleteMyResult,
-  getInstituteResults,
-  getAdminInstituteResults,
-  deleteAdminResult,
-  createInstituteResultPremium,
-  createInstituteAdmission,
-  getInstituteAdmissions,
-  deleteMyAdmission,
-  getAdminInstituteAdmissions,
-  deleteAdminAdmission,
-  createInstituteAdmissionPremium
+  getAdminQuestions
 } = require("../controllers/questionBankController");
 const { isAuthenticatedCandidate, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/institutes").get(getAllInstitutes);
+router.route("/question/:id").get(getQuestion);
+
+router.route("/allQuestions").get(getAllQuestions);
 
 router
-  .route("/admin/institutes")
-  .get(isAuthenticatedCandidate, getAdminInstitutes);
+  .route("/admin/questions")
+  .get(isAuthenticatedCandidate, getAdminQuestions);
 
 router
   .route("/admin/questionBank/new")
   .post(isAuthenticatedCandidate, createQuestionBank);
 
 router
-  .route("/admin/institute/:id")
-  .put(isAuthenticatedCandidate, updateInstitute)
-  .delete(isAuthenticatedCandidate, authorizeRoles("admin"), deleteInstitute);
+  .route("/admin/question/:id")
+  .put(isAuthenticatedCandidate, authorizeRoles("admin"), updateQuestionAdmin)
+  .delete(isAuthenticatedCandidate, authorizeRoles("admin"), deleteQuestion);
 
-router.route("/institute/:id").get(getInstituteDetails);
+router.route("/question/review").put(isAuthenticatedCandidate, createQuestionReview);
 
-router.route("/institute/review").put(isAuthenticatedCandidate, createInstituteReview);
-
-router.route("/institute/reviews/:id").get(instituteReviews);
+router.route("/question/reviews/:id").get(questionReviews);
   
-router.route("/institute/reviews").delete(isAuthenticatedCandidate, deleteReview);
-
-router.route("/institute/result/new").post(isAuthenticatedCandidate, createInstituteResult).get(isAuthenticatedCandidate, getInstituteResults);
-
-router.route("/institute/result/:id").put(isAuthenticatedCandidate, deleteMyResult);
-
-router.route("/institute/admin/result/:id").get(isAuthenticatedCandidate, getAdminInstituteResults);
-
-router.route("/institute/admin/delete/result").put(isAuthenticatedCandidate, deleteAdminResult);
-
-router.route("/institute/result/premium").post(isAuthenticatedCandidate, createInstituteResultPremium);
-
-router.route("/institute/admission/new").post(isAuthenticatedCandidate, createInstituteAdmission).get(isAuthenticatedCandidate, getInstituteAdmissions);
-
-router.route("/institute/admission/:id").put(isAuthenticatedCandidate, deleteMyAdmission);
-
-router.route("/institute/admin/admissions/:id").get(isAuthenticatedCandidate, getAdminInstituteAdmissions);
-
-router.route("/institute/admin/delete/admission").put(isAuthenticatedCandidate, deleteAdminAdmission);
-
-router.route("/institute/admission/premium").post(isAuthenticatedCandidate, createInstituteAdmissionPremium);
+router.route("/question/reviews").delete(isAuthenticatedCandidate, deleteReview);
 
 module.exports = router;
